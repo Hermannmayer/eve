17842bc666dba1b44f8eaabaefeff7a8ba464bd6
2292527883 <2292527883@qq.com>
2025-05-22 22:39:29 +0800

    get id

--- ctldata.py
+++ ctldata.py
@@ -26,17 +26,21 @@ def create_tables():
     conn.commit()
     conn.close()
 def getidapi():
-    idapi='https://sde.jita.space/universe/types'
+    idapi='https://sde.jita.space/latest/universe/types'
     response=requests.get(idapi)
-    if response.status_code != 200:
-        print(f"Failed to fetch data: {response.status_code}")
-        return None
+    type_ids=response.json()
+    conn=sqlite3.connect('./Datas/iteamdata.db')
+    cursor=conn.cursor()
+    insert_count = 0
+    for type_id in type_ids:
+        try:
+            cursor.execute('INSERT INTO item (type_id) VALUES (?)', (type_id,))
+            insert_count += 1
+        except sqlite3.IntegrityError:
+            pass
+    conn.commit()
+    conn.close()    
+    print(f"Inserted {insert_count} type IDs into the database.")
 
-
-    #cursor.execute("""
-
-
-#""")
-
- 
-
+    
+getidapi()
